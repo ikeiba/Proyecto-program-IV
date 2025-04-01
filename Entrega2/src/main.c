a161963de1d86754a07b3f2d9f54624154ef7bba
Asier Gómez Viteri <asier.gomez@opendeusto.es>
2025-04-01 18:47:09 +0200

    main update

--- Entrega2/src/main.c
+++ Entrega2/src/main.c
@@ -46,30 +46,5 @@ int main(){
     //menuMorrarLog();
 
     administracion();
-
-    /*
-    Config configData;
-    char usuario[TAM], clave[TAM];
-
-    configData = leerConfig("configuracion.config");
-    printf("Usuario Administrador: %s\n", configData.administrador);
-    printf("Clave de Acceso: %s\n", configData.contrasena);
-    printf("Introduce el nombre del usuario: ");
-    gets(usuario);
-    if (strcmp(usuario, configData.administrador) == 0) {
-        printf("Ingrese su clave: ");
-        gets(clave);
-
-        if (strcmp(clave, configData.contrasena) == 0) {
-            printf("Acceso concedido\n");
-            //writeLog("Autenticación exitosa");
-        } else {
-            printf("Clave incorrecta\n");
-            //writeLog("Error de autenticación: clave incorrecta");
-        }
-    } else {
-        printf("Usuario no reconocido\n");
-        //writeLog("Error de autenticación: usuario incorrecto");
-    }*/
     return 0;
 } 
