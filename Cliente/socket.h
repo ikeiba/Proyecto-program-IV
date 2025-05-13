6295232267d67c5b81ac3ff8e104a7c9fb66deb6
Asier Gómez Viteri <asier.gomez@opendeusto.es>
2025-05-13 10:13:08 +0200

    quitar extend cliente

--- Cliente/socket.h
+++ Cliente/socket.h
@@ -1,15 +1,7 @@
 #ifndef _SOCKET_H_
 #define _SOCKET_H_
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 int inicializarSocket();
 int inicioSesion(const char* usuario, const char* contrasenya);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
