9fe2a81fa8aca830cc3a87bec11b6ebea852d3cd
ikeiba <ibarrola.iker@opendeusto.es>
2025-05-19 12:35:58 +0200

    add update to main

--- Servidor/main.c
+++ Servidor/main.c
@@ -123,6 +123,10 @@ int main(int argc, char *argv[]) {
                     gestionarMensajeGET(sendBuff, recvBuff, &comm_socket);
                 }
 
+                if(strcmp(tipo, "UPDATE") == 0){
+                    gestionarMensajeUPDATE(sendBuff, recvBuff, &comm_socket);
+                }
+
                 if (strcmp(tipo, "Bye") == 0){
                     break;
                 }       
