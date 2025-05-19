1ee802d593e01c84252fb477565d69f76737db4d
XabiVii <xabier.villa@opendeusto.es>
2025-05-19 21:07:00 +0200

    Cambios

--- Cliente/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Proyecto-program-IV_FILE C:/prog4/Proyecto-program-IV/build/Proyecto-program-IV.exe)
-set(__QT_DEPLOY_TARGET_Proyecto-program-IV_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Proyecto-program-IV_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
